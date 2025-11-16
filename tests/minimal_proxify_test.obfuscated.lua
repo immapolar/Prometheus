a1ce869f30b6a0d4ac8c32c861818f0caef4e4d0
Polaris <belalisam999@gmail.com>
2025-11-16 22:35:28 +0200

    Minimal Proxify Test Obfuscated Addition

---
+++ tests/minimal_proxify_test.obfuscated.lua
@@ -0,0 +1,22 @@
+local oz5YDapg9Qz5YDa = setmetatable;
+local g = function()
+ 
+	end;
+local R = oz5YDapg9Qz5YDa({ bw = oz5YDapg9Qz5YDa({ JW = oz5YDapg9Qz5YDa({ bR = oz5YDapg9Qz5YDa({ hx = 5 }, { __pow = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+						HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.hx = mDapg9Qz;
+					end, __unm = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q)
+						return HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.hx;
+					end }) }, { __div = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+					g(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.bR ^ mDapg9Qz);
+				end, __unm = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q)
+					return -HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.bR;
+				end }) }, { __add = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+				g(rawget(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, "\074\087") / mDapg9Qz);
+			end, __index = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+				return (rawget(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, "\074\087"))[mDapg9Qz];
+			end }) }, { __div = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+			g(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.bw + mDapg9Qz);
+		end, __sub = function(HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q, mDapg9Qz)
+			return HYDapg9Qz5YDapg9Qz5YDapg9Qz5YDapg9Q.bw[-4778898];
+		end });
+print(R - (-4606349));
